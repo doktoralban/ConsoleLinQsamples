cca070cd2a7056a53dc6b99d212ae33f47f66859
Aydın Türker <doktoralban@outlook.com>
2021-07-08 10:03:49 +0300

    Add project files.

---
+++ ConsoleLinQsamples/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLinQsamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultipleWhereClauses();
+
+
+        }
+
+
+
+
+
+        static void MultipleWhereClauses()
+            {
+            var names = new[] { "Tom", "Dick", "Harry", "Mary", "Jay" }.AsQueryable();
+
+            var query =
+                from n in names
+                where n.Length > 3
+                let u = n.ToUpper()
+                where u.EndsWith("Y")
+                select u;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+
+
+
+    }
+}
