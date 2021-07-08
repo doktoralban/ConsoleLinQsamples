96ca64f55e64f3e6b1da3d38ac3f637b4d970036
Aydın Türker <doktoralban@outlook.com>
2021-07-08 10:33:32 +0300

    ek.

--- ConsoleLinQsamples/Program.cs
+++ ConsoleLinQsamples/Program.cs
@@ -10,15 +10,14 @@ namespace ConsoleLinQsamples
     {
         static void Main(string[] args)
         {
-            MultipleWhereClauses();
+            //MultipleWhereClauses();
+            //SampleWhereMin();
+            TakeWhileSkipwhile();
+        } 
 
-
-        }
-
-
-
-
-
+        /// <summary>
+        /// ilk where( n.Length > 3) den sonra toupper onun içinden endswith
+        /// </summary>
         static void MultipleWhereClauses()
             {
             var names = new[] { "Tom", "Dick", "Harry", "Mary", "Jay" }.AsQueryable();
@@ -34,8 +33,51 @@ namespace ConsoleLinQsamples
             {
                 Console.WriteLine(item);
             }
+        }
 
 
+        /// <summary>
+        /// dizideki eleman sayısı en az olanı bulup o sayı kadar elemanı olanlar
+        /// </summary>
+        static void SampleWhereMin()
+        {
+            var names = new[] { "Tom", "Dick", "Harry", "Mary", "Jay" }.AsQueryable();
+            IQueryable<string> queryable = (
+                from n in names
+                where n.Length == names.Min(n2 => n2.Length)
+                select n
+            );
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        /// <summary>
+        /// TakeWhile = belirlenen(örnekte : 100)den fazla olan(Hariç: örnekte: 234)a gelene kadar öncekiler(örnekte: 3,5,2).
+        /// Skipwhile = belirlenen(örnekte : 100)den fazla olanDan(Dahil: örnekte: 234) itibaren sonrakiler (örnekte: 234,4,1)
+        /// </summary>
+        static void TakeWhileSkipwhile()
+        {
+            int[] numbers = { 3, 5, 2, 234, 4, 1 };
+
+           var a1= numbers.TakeWhile(n => n < 100) ;
+
+            foreach (var item in a1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(" --- --- --- --- --- --- ");
+
+            var a2 =numbers.SkipWhile(n => n < 100) ;
+
+            foreach (var item in a2)
+            {
+                Console.WriteLine(item);
+            }
 
         }
 
